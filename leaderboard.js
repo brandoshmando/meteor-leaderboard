916d9b44fd6f7c4708dc4a536372840e2c6d9b0c
brandoshmando <brancraft@gmail.com>
2014-08-04 22:51:36 -0400

    Changed for loop to for each so that I could capture the document and then pass it in to be updated. And that worked...

--- leaderboard.js
+++ leaderboard.js
@@ -36,10 +36,9 @@ if (Meteor.isClient) {
     },
 
     'click input.random' : function(){
-      var players = Players.find({});
-      for (var i=0; i < players.count; i++){
-        players[i].update({ $set: { score: Math.floor(Random.fraction()*10)*5}});
-      };
+      Players.find({}).forEach(function(player){
+        Players.update( {_id: player._id}, { $set: { score: Math.floor(Random.fraction()*10)*5}});
+      });
     }
   });
 
@@ -51,17 +50,22 @@ if (Meteor.isClient) {
 }
 
 // On server startup, create some players if the database is empty.
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace",
-                   "Grace Hopper",
-                   "Marie Curie",
-                   "Carl Friedrich Gauss",
-                   "Nikola Tesla",
-                   "Claude Shannon"];
-      for (var i = 0; i < names.length; i++)
-        Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
-    }
-  });
-}
+// if (Meteor.isServer) {
+//   Meteor.startup(function () {
+//     if (Players.find().count() === 0) {
+//       var names = ["Ada Lovelace",
+//                    "Grace Hopper",
+//                    "Marie Curie",
+//                    "Carl Friedrich Gauss",
+//                    "Nikola Tesla",
+//                    "Claude Shannon"];
+//       for (var i = 0; i < names.length; i++)
+//         Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
+//     }
+//   });
+// }
+
+ // var players = Players.find({});
+ //  for (var i=0; i < players.count; i++){
+ //    players[i].update({ $set: { score: Math.floor(Random.fraction()*10)*5}});
+ //  };
